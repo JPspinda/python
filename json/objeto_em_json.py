44535a0b10791f2e71f174ac3949fae2ce1a676a
JPspinda <jppaulinospinda@gmail.com>
2025-05-23 14:21:38 -0300

    Programação Orientada a Objeto

---
+++ json/objeto_em_json.py
@@ -0,0 +1,20 @@
+import json
+
+CAMINHO_ARQUIVO = '.objeto_em_json.json'
+
+class Pessoa:
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+        
+p1 = Pessoa('João', 33)
+p2 = Pessoa('Helena', 21)
+p3 = Pessoa('Joana', 11)
+bd = [vars(p1), p2.__dict__, vars(p3)]
+
+def fazer_dump():
+    with open(CAMINHO_ARQUIVO, 'w', encoding='utf8') as arquivo:
+        dado = json.dump(bd, arquivo, indent=2, ensure_ascii=False)
+    
+if __name__ == '__main__':
+    fazer_dump()
